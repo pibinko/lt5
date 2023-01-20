b2cf951664ea8afbd92cfc081ac10b8b88ebcc46
pibinko <pibinko@gmail.com>
2023-01-20 08:54:17 +0100

    new sheet link

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1a-ufDnoF376wBF9cQXiF5HNx4eAUYq2pkqzBMKCPhEM/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1Wuqh4jUZ7i2Ew9GLQmBpiNNQSBb8C5CXuZpoVCkPez4/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyCVrszP2lHRxk89c6UNvzLEcqvaYiu1M3s';
