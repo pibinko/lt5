0b7196c618c9350ea3d8e1a1e24ac228059181d0
pibinko <pibinko@gmail.com>
2023-01-20 08:21:32 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1a-ufDnoF376wBF9cQXiF5HNx4eAUYq2pkqzBMKCPhEM/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
+var googleApiKey = 'AIzaSyCVrszP2lHRxk89c6UNvzLEcqvaYiu1M3s';
